51aa822498d26a2ddfb39c4d05de060fefdd89c8
Rami Nakkar <rami.nakkar@miragon.io>
2023-06-09 12:22:47 +0200

    feat: add WorkerAdapter

---
+++ examples/library-process/library-process-core/src/main/java/io/miragon/libraryprocess/adapter/in/worker/WorkerAdapter.java
@@ -0,0 +1,26 @@
+package io.miragon.libraryprocess.adapter.in.worker;
+
+import io.miragon.libraryprocess.application.port.in.checkbookavailablity.CheckBookAvailabilityCommand;
+import io.miragon.libraryprocess.application.port.in.checkbookavailablity.CheckBookAvailabilityResult;
+import io.miragon.libraryprocess.application.port.in.checkbookavailablity.CheckBookAvailabilityUseCase;
+import io.miragon.libraryprocess.application.port.in.prepareshipment.PrepareShipmentCommand;
+import io.miragon.libraryprocess.application.port.in.prepareshipment.PrepareShipmentUseCase;
+import io.miragon.servicetaskapi.api.Worker;
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public class WorkerAdapter {
+
+    private final CheckBookAvailabilityUseCase checkBookAvailabilityUseCase;
+    private final PrepareShipmentUseCase prepareShipmentUseCase;
+
+    @Worker
+    public CheckBookAvailabilityResult checkAvailability(CheckBookAvailabilityCommand checkBookAvailabilityCommand) {
+        return checkBookAvailabilityUseCase.checkAvailability(checkBookAvailabilityCommand);
+    }
+
+    @Worker
+    public void prepareShipment(PrepareShipmentCommand prepareShipmentCommand) {
+        prepareShipmentUseCase.prepareShipment(prepareShipmentCommand);
+    }
+}
