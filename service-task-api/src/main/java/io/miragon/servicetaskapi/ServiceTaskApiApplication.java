d86f92d297d7d54b1ffff9638ebacc2e9172c6cd
Rami Nakkar <rami.nakkar@miragon.io>
2023-06-09 10:32:47 +0200

    feat: add init serivce-task-api

---
+++ service-task-api/src/main/java/io/miragon/servicetaskapi/ServiceTaskApiApplication.java
@@ -0,0 +1,13 @@
+package io.miragon.servicetaskapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceTaskApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceTaskApiApplication.class, args);
+	}
+
+}
---
+++ service-task-api/src/test/java/io/miragon/servicetaskapi/ServiceTaskApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.miragon.servicetaskapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceTaskApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
