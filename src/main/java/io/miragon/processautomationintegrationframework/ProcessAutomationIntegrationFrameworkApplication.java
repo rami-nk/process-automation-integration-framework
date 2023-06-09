bd6bee4e318690bf450686d7e3c3685bae2dcbc1
Rami Nakkar <rami.nakkar@miragon.io>
2023-06-09 10:30:49 +0200

    init commit

---
+++ src/main/java/io/miragon/processautomationintegrationframework/ProcessAutomationIntegrationFrameworkApplication.java
@@ -0,0 +1,13 @@
+package io.miragon.processautomationintegrationframework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProcessAutomationIntegrationFrameworkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProcessAutomationIntegrationFrameworkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/miragon/processautomationintegrationframework/ProcessAutomationIntegrationFrameworkApplicationTests.java
@@ -0,0 +1,13 @@
+package io.miragon.processautomationintegrationframework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProcessAutomationIntegrationFrameworkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
